179aaacce6dd933f941497b1b20c10240b8a0826
Benjamin Anderson <andersonbcdefg@gmail.com>
2023-03-21 15:15:44 -0700

    separate test for speedup

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import time
 import torch
+from tqdm.auto import tqdm
 from model import GPT, GPTConfig
 
 def test_model(config_path="config/medium.yaml"):
@@ -19,14 +20,6 @@ def test_model(config_path="config/medium.yaml"):
     # delete to save memory
     del out1
 
-    print("Testing speed after warmup.")
-    times = []
-    for i in range(30):
-        start = time.time()
-        out = model(example_input)
-        times.append(time.time() - start)
-        del out
-    print("Average forward pass time after 25 warmup batches:", sum(times[-5:]) / 5.0)
     print("Model forward pass test passed.")
 
 def test_compiled_model(config_path="config/medium.yaml"):
@@ -48,21 +41,55 @@ def test_compiled_model(config_path="config/medium.yaml"):
     assert out1.shape == (8, config.seq_len, config.vocab_size), "Output shape is incorrect."
     # delete to save memory
     del out1
-
-    print("Testing speed after warmup.")
-    times = []
-    for i in range(30):
-        start = time.time()
-        out = compiled(example_input)
-        times.append(time.time() - start)
-        del out
-    print("Average forward pass time after 25 warmup batches:", sum(times[-5:]) / 5.0)
     print("Compiled model test passed.")
-    
 
+# from pytorch docs
+def timed(fn):
+    start = torch.cuda.Event(enable_timing=True)
+    end = torch.cuda.Event(enable_timing=True)
+    start.record()
+    result = fn()
+    end.record()
+    torch.cuda.synchronize()
+    return result, start.elapsed_time(end) / 1000
+
+def run_forward_passes(config, model, device, num_passes=1000):
+    times = []
+    for i in tqdm(range(num_passes)):
+        random_tensor = torch.randint(0, config.vocab_size, (4, config.seq_len))
+        random_tensor = random_tensor.to(device)
+        out, time = timed(lambda: model(random_tensor))
+        times.append(time)
+    return times
+
+def test_compiled_speedup(config_path="config/medium.yaml"):
+    # Create model and compiled model
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(f"Instantiating model from {config_path}.")
+    config = GPTConfig.from_yaml(config_path)
+    model = GPT(config)
+    model.to(device)
+    compiled = torch.compile(model, mode="default")
+    print("torch.compile() ran successfully.")
+    
+    # Run forward passes on normal model
+    print("Warming up...")
+    run_forward_passes(config, model, device, num_passes=100)
+    print("Testing normal forward pass.")
+    normal_times = run_forward_passes(config, model, device)
+    print(torch.mean(torch.tensor(normal_times)).item())
+
+    # Run forward passes on compiled model
+    print("Warming up...")
+    run_forward_passes(config, compiled, device, num_passes=100)
+    print("Testing compiled forward pass.")
+    compiled_times = run_forward_passes(config, compiled, device)
+    print(torch.mean(torch.tensor(compiled_times)).item())
+    
 if __name__ == "__main__":
     test_model()
     if torch.cuda.is_available():
         test_compiled_model()
+        test_compiled_speedup()
     else:
-        print("Skipping compiled model test because CUDA is not available.")
+        print("Skipping compiled model tests because CUDA is not available.")
