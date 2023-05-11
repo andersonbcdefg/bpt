fixed causal attention block to not be buggy

model.py
@@ -5,42 +5,6 @@ import torch
 from torch.utils.checkpoint import checkpoint_sequential
 from torch import nn
 from torch.nn import functional as F
-from dataclasses import dataclass
-
-@dataclass
-class GPTConfig:
-  vocab_size: int
-  seq_len: int
-  d_model: int
-  d_ffn: int
-  d_qkv: int
-  n_heads: int
-  n_layers: int
-  dropout: float
-  tie_weights: bool
-  fused_transformer_block: bool = True
-  rotary_emb: bool = True
-  rotary_pct: float = 0.25
-  use_xpos: bool = True
-  xpos_scale_base: int = 512
-  stable_embedding: bool = False
-  checkpointing: bool = True
-  
-
-  def __post_init__(self):
-    if self.d_qkv != self.d_model // self.n_heads:
-      warnings.warn("d_qkv is not equal to d_model // n_heads. This is ok, but unusual.")
-
-  @classmethod
-  def from_yaml(cls, path):
-    with open(path, 'r') as f:
-      return cls(**yaml.safe_load(f))
-
-  def __repr__(self):
-    return yaml.dump(self.__dict__)
-
-  def __str__(self):
-    return yaml.dump(self.__dict__)
 
 class RMSNorm(nn.Module):
   def __init__(self, dim, eps = 1e-8):
@@ -55,8 +19,6 @@ class RMSNorm(nn.Module):
 
 ## Rotary embedding adapted from Phil Wang (lucidrains) Github repo `palm_rlhf_pytorch`.
 # I modify the apply_rotary_pos_emb function to allow rotating only a fraction of the input tensor.
-
-
 @torch.jit.script
 def rotate_half(x):
     x1, x2 = x.chunk(2, dim=-1)
@@ -98,14 +60,17 @@ class CausalAttention(nn.Module):
   def __init__(self, config):
     super().__init__()
     self.W_qkv = nn.Linear(config.d_model, config.n_heads * config.d_qkv * 3, bias=False)
-    self.rotary_emb = RotaryEmbedding(int(config.d_qkv * config.rotary_pct), scale_base=config.xpos_scale_base, use_xpos=config.use_xpos)
     self.dropout_p = config.dropout
     self.out_proj = nn.Linear(config.d_qkv * config.n_heads, config.d_model, bias=False)
     self.d_qkv = config.d_qkv
     self.n_heads = config.n_heads
 
-    self.register_buffer("pos_emb", None, persistent=False)
-    self.register_buffer("pos_emb_scale", None, persistent=False)
+    if config.rotary_emb:
+      self.rotary_emb = RotaryEmbedding(int(config.d_qkv * config.rotary_pct), scale_base=config.xpos_scale_base, use_xpos=config.use_xpos)
+      self.register_buffer("pos_emb", None, persistent=False)
+      self.register_buffer("pos_emb_scale", None, persistent=False)
+    else:
+      self.rotary_emb = None
 
   def get_rotary_embedding(self, n, device):
     if self.pos_emb is not None and self.pos_emb.shape[-2] >= n:
@@ -117,15 +82,15 @@ class CausalAttention(nn.Module):
     return pos_emb, scale
   
   def forward(self, x):
-    seq_len, device = x.shape[1], X.device
+    seq_len, device = x.shape[1], x.device
     qkv = self.W_qkv(x) # b, l, d_qkv * n_heads * 3
     new_qkv_shape = qkv.size()[:-1] + (self.n_heads, 3 * self.d_qkv) # b, l, h, d_qkv * 3
     q, k, v = qkv.view(*new_qkv_shape).permute(0, 2, 1, 3).chunk(3, dim=-1)
 
-    positions, scale = self.get_rotary_embedding(seq_len, device)
-
-    q = apply_rotary_pos_emb(positions, q, scale)
-    k = apply_rotary_pos_emb(positions, k, scale ** -1)
+    if self.rotary_emb:
+      positions, scale = self.get_rotary_embedding(seq_len, device)
+      q = apply_rotary_pos_emb(positions, q, scale)
+      k = apply_rotary_pos_emb(positions, k, scale ** -1)
     attn_out = F.scaled_dot_product_attention(q, k, v, dropout_p=self.dropout_p, is_causal=True).transpose(1, 2) # b, l, h, d_attn
     return self.out_proj(attn_out.flatten(2, 3))
 

train.py
@@ -36,6 +36,7 @@ def train(
     tokenizer_path="EleutherAI/pythia-1.4b",
     train_dataset="andersonbcdefg/minipile_train_tokenized",
     val_dataset="andersonbcdefg/minipile_val_tokenized",
+    pre_tokenized=True,
     max_batch_size = 2048,
     micro_batch_size = 32,
     initial_batch_size = 256,
@@ -48,8 +49,9 @@ def train(
     anneal_strategy = "linear", # "cosine" is the other option
     optim_8bit = False,
     precision="bf16",
-    grad_clip = None
-
+    grad_clip = None,
+    save_every = 10000,
+    ckpt_dir = "/storage"
 ):
     # make it so that we specify either my GPT or gpt_neox in the config and load accordingly
     config = GPTConfig.from_yaml(config_path)
@@ -57,17 +59,22 @@ def train(
     tokenizer = AutoTokenizer.from_pretrained(tokenizer_path)
     tokenizer.pad_token = tokenizer.eos_token
 
-    minipile_train = load_dataset("andersonbcdefg/minipile_train_tokenized", split="train")
-    minipile_train.set_format("pt")
-    minipile_val = load_dataset("andersonbcdefg/minipile_val_tokenized", split="validation")
-    minipile_val.set_format("pt")
+    train_dataset = load_dataset("andersonbcdefg/minipile_train_tokenized", split="train")
+    train_dataset.set_format("pt")
+    if val_dataset is not None:
+        val_dataset = load_dataset("andersonbcdefg/minipile_val_tokenized", split="validation")
+        val_dataset.set_format("pt")
+    
+    if not pre_tokenized:
+        train_dataset = train_dataset.map(get_tokenize_fn(tokenizer, config.seq_len), batched=True, remove_columns=train_dataset.column_names)
+        if val_dataset is not None:
+            val_dataset = val_dataset.map(get_tokenize_fn(tokenizer, config.seq_len), batched=True, remove_columns=val_dataset.column_names)
 
-    train_dataloader = torch.utils.data.DataLoader(minipile_train, batch_size=micro_batch_size, shuffle=True, num_workers=4, pin_memory=True) #, collate_fn=collate_fn)
-    val_dataloader = torch.utils.data.DataLoader(minipile_val, batch_size=micro_batch_size, shuffle=False) #, collate_fn=collate_fn)
+    train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=micro_batch_size, shuffle=True, num_workers=4, pin_memory=True) #, collate_fn=collate_fn)
+    val_dataloader = torch.utils.data.DataLoader(val_dataset, batch_size=micro_batch_size, shuffle=False) #, collate_fn=collate_fn)
 
     # Model, optimizer, scheduler
     model.to(torch.device("cuda"))
-    max_lr = 3.0e-4
     if optim_8bit:
         if config.stable_embedding == False:
             warnings.warn("You are using 8-bit AdamW but stable embedding is not enabled. This is not recommended.")
@@ -149,13 +156,14 @@ def train(
                 "optimizer_steps": optimizer_steps
             })
             # take 16 steps at each effective batch size before increasing it (to speed initial learning)
-            if gradient_accumulation_steps < (max_batch_size // micro_batch_size) and optimizer_steps % 16 == 0:
+            if gradient_accumulation_steps < (max_batch_size // micro_batch_size) and optimizer_steps % steps_per_batch_size == 0:
                 gradient_accumulation_steps += 1
         
         if (index + 1) % save_every == 0:
             print("Saving checkpoint...")
-            accelerator.save_state("/storage")
+            accelerator.save_state(ckpt_dir)
         
         scheduler.step()    
     
-
+if __name__ == "__main__":
+    fire.Fire(train)